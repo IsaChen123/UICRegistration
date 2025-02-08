4717694f476c36ee210e58ab32eaeae54d6baf6f
Fez <Fernandolpz0911.business@Gmail.com>
2025-02-08 12:04:57 -0600

    added a button

--- functs.js
+++ functs.js
@@ -6,3 +6,8 @@ function on() {
 function off () {
     document.getElementById("overlay").style.display = "none";
 }
+
+// REDIRECT TO PAGE AFTER SEARCH CLICKED
+document.getElementById("searchAgain").addEventListener("click", function() {
+    window.location.href = "register-for-classes.html";
+});
