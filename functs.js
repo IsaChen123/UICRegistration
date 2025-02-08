6b71d0249bfdcb46fb8b0009a08b0aa86884932c
IsaChen123 <isabellachen123@gmail.com>
2025-02-08 07:01:15 -0600

    started programmatcially creating the display for classes

--- functs.js
+++ functs.js
@@ -6,3 +6,21 @@ function on() {
 function off () {
     document.getElementById("overlay").style.display = "none";
 }
+
+function displayMatchingClasses(){
+    done=false;
+    while(!done){
+        let container=document.createElement("div");
+        document.getElementById("tabcontent").appendChild(container);
+        let table=document.createElement("table");
+        container.appendChild(table);
+        let row1=document.createElement("tr");
+        table.appendChild(row1);
+        let col1=document.createElement("td")
+        row1.appendChild(col1);
+        let className=document.createElement("p")
+        className.textContent=SearchParams.name
+        let professorName=document.createElement("p")
+        professorName.append()
+    }
+}
